9ca130a21bbfa42448346a2da566c3eb5c3b4f28
Victor Cheeney <victorcheeney@gmail.com>
2022-03-08 05:39:04 -0500

    Removed console.los

--- algos/quicksort.test.ts
+++ algos/quicksort.test.ts
@@ -52,11 +52,7 @@ describe("quicksort", () => {
     ];
 
     quicksortRecursive(arr);
-    console.log(arr);
     for (let i = 0; i + 1 < arr.length; i++) {
-      if (arr[i] > arr[i + 1]) {
-        console.log(i);
-      }
       expect(arr[i] <= arr[i + 1]).toBe(true);
     }
   });
